9816019ac1c887e388800befd2b2af9c478a8e6f
kmchaejin <chaejin040@gmail.com>
2025-03-25 18:41:37 +0900

    chore: 공백 제거

--- src/main/java/com/example/todoproject/service/TodoServiceImpl.java
+++ src/main/java/com/example/todoproject/service/TodoServiceImpl.java
@@ -7,7 +7,6 @@ import com.example.todoproject.repository.TodoRepository;
 import com.example.todoproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
